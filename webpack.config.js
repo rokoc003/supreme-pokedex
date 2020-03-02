34853fca0e23ff13ba42c510f807d36047fabf8e
rokoc003 <roxane.kocem@yahoo.de>
2020-03-02 15:32:23 +0100

    Add webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Supreme-Pokedex'
+    })
+  ],
+  devServer: {
+    contentBase: './dist'
   }
 };
