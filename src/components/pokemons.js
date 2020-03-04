4e9764327d3b77f4e6ee433ea058a03f2f73f12e
rokoc003 <roxane.kocem@yahoo.de>
2020-03-04 16:47:29 +0100

    Add click event to pokemon elements

--- src/components/pokemons.js
+++ src/components/pokemons.js
@@ -11,6 +11,11 @@ export function createPokemons(items) {
       innerText: item,
       className: 'pokemon'
     });
+    element.addEventListener('click', () => {
+      const favorites = [item];
+      const favoritesJSON = JSON.stringify(favorites);
+      localStorage.setItem('favorites', favoritesJSON);
+    });
     appendContent(container, element);
   });
   return container;
