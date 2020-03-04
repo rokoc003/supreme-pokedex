438f055831e4d119aed043c0f09b6ee2b3d966de
rokoc003 <roxane.kocem@yahoo.de>
2020-03-04 17:22:38 +0100

    Fix typo

--- src/components/pokemons.js
+++ src/components/pokemons.js
@@ -12,7 +12,7 @@ export function createPokemons(items) {
       className: 'pokemon'
     });
     element.addEventListener('click', () => {
-      const favorites = JSON.parse(localStorage.getItem('favories')) || [];
+      const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
       favorites.push(item);
       const favoritesJSON = JSON.stringify(favorites);
       localStorage.setItem('favorites', favoritesJSON);
